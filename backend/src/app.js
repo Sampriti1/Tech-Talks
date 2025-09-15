ca4aa44b1dfaf52ee72e8b7b3f7825f47db78da1
Sampriti1 <sampritik100@gmail.com>
2025-09-15 20:56:50 +0530

    updated code

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import router from "./Routes/authRoutes.js";
 import blogRoutes from "./Routes/blogRoutes.js"
+import { fileURLToPath } from "url";
+import path from "path";
 dotenv.config();
 export const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.json());
 
 app.use(
@@ -19,6 +23,11 @@ app.get("/api/health", (_, res) => res.json({ ok: true}));
 app.use('/uploads', express.static('uploads'));
 app.use("/api", router);
 app.use("/api/blogs", blogRoutes);
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 
 app.use((req, res) => res.status(404).json({message: "Not found"}));
